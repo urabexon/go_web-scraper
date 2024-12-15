e722a6f1dae3cbd3c4652cc434e44bd868c2dd13
urabexon <hiroki708.a48uk@gmail.com>
2024-12-15 16:48:18 +0900

    init

---
+++ web_scraper.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"time"
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+
+}
+
+func download(url string) (err error) {
+	
+}
